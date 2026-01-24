2d4773929f2466de3d26e3a847d5ed181ca6b65d
itallume <itallo.oliver21@gmail.com>
2026-01-23 21:32:08 -0300

    etapa 4

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,23 +2,34 @@ package payment_adapter
 
 import (
 	"context"
-	
+	"log"
+	"time"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
 	"github.com/itallume/microservices-proto/golang/payment"
 	"github.com/itallume/microservices/order/internal/application/core/domain"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Adapter struct {
 	payment payment.PaymentClient
 }
 
-func NewAdapter(paymentServiceUrl string) (*Adapter, error){
+func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(retry.UnaryClientInterceptor(
+			retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			retry.WithMax(5),
+			retry.WithBackoff(retry.BackoffLinear(time.Second)),
+		)))
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -26,12 +37,19 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error){
 	return &Adapter{payment: client}, nil
 }
 
-func (a *Adapter) Charge(order *domain.Order) (error){
-	_, err := a.payment.Create(context.Background(), 
-	&payment.CreatePaymentRequest{
-		UserId: order.CustomerID,
-		OrderId: order.ID,
-		TotalPrice: order.TotalPrice(),
-		}) 
+func (a *Adapter) Charge(order *domain.Order) error {
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	_, err := a.payment.Create(ctx,
+		&payment.CreatePaymentRequest{
+			UserId:     order.CustomerID,
+			OrderId:    order.ID,
+			TotalPrice: order.TotalPrice(),
+		})
+
+	code := status.Code(err)
+	if code == codes.DeadlineExceeded {
+		log.Printf("2-second deadline exceeded")
+		return status.New(codes.DeadlineExceeded, "2-second deadline exceeded").Err()
+	}
 	return err
 }
